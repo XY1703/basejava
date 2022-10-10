cad5ac8de3feb9982a1cf03e0da6c10e56ee098c
XY1703 <java.ay@mail.ru>
2022-10-10 15:35:49 +0300

    HW 1 ArrayStorage methods was initialized

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,52 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
 
     void save(Resume r) {
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume r = new Resume();
+
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) r = storage[i];
+        }
+
+        return r;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resumes = new Resume[size];
+
+        if (size >= 0) System.arraycopy(storage, 0, resumes, 0, size);
+
+        return resumes;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
