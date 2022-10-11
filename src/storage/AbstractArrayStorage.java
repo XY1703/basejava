c833a7844391ce628f67aed399d36a13e860598f
XY1703 <java.ay@mail.ru>
2022-10-11 17:43:26 +0300

    HW 3 done

--- src/storage/AbstractArrayStorage.java
+++ src/storage/AbstractArrayStorage.java
@@ -2,6 +2,8 @@ package storage;
 
 import model.Resume;
 
+import java.util.Arrays;
+
 public abstract class AbstractArrayStorage implements Storage{
 
     protected static final int STORAGE_LIMIT = 10000;
@@ -21,6 +23,60 @@ public abstract class AbstractArrayStorage implements Storage{
         }
         return storage[index];
     }
+
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    public void update(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index == -1) {
+            System.out.println("Resume " + r.getUuid() + " not exist");
+        } else {
+            storage[index] = r;
+        }
+    }
+
+    public void save(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index >= 0) {
+            System.out.println("Resume " + r.getUuid() + " already exist");
+        } else if (size == STORAGE_LIMIT) {
+            System.out.println("Storage overflow");
+        } else {
+            insertElement(r, index);
+            size++;
+        }
+    }
+
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index < 0) {
+            System.out.println("Resume " + uuid + " not exist");
+        } else {
+            fillDeletedElement(index);
+            storage[size - 1] = null;
+            size--;
+        }
+    }
+
+    protected abstract void fillDeletedElement(int index);
+
+    protected abstract void insertElement(Resume r, int index);
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    public Resume[] getAll() {
+        Resume[] resumes = new Resume[size];
+
+        if (size >= 0) System.arraycopy(storage, 0, resumes, 0, size);
+
+        return resumes;
+    }
+
+
 
     protected abstract int getIndex(String uuid);
 }
--- src/storage/ArrayStorage.java
+++ src/storage/ArrayStorage.java
@@ -2,8 +2,6 @@ package storage;
 
 import model.Resume;
 
-import java.util.Arrays;
-
 /**
  * Array based storage for Resumes
  */
@@ -11,55 +9,15 @@ public class ArrayStorage extends AbstractArrayStorage {
     Resume[] storage = new Resume[10000];
     private int size = 0;
 
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
+
+    @Override
+    protected void fillDeletedElement(int index) {
+        storage[index] = storage[size - 1];
     }
 
-    public void save(Resume r) {
-        if (getIndex(r.getUuid()) != -1) {
-            System.out.println("Resume " + r.getUuid() + " already exist");
-        } else if (size >= STORAGE_LIMIT) {
-            System.out.println("Storage overflow");
-        } else {
-            storage[size] = r;
-            size++;
-        }
-    }
-
-    public void delete(String uuid) {
-        int index = getIndex(uuid);
-        if (index == -1) {
-            System.out.println("Resume " + uuid + " not exist");
-        } else {
-            storage[index] = storage[size - 1];
-            storage[size - 1] = null;
-            size--;
-        }
-    }
-
-    public void update(Resume r) {
-        int index = getIndex(r.getUuid());
-        if (index == -1) {
-            System.out.println("Resume " + r.getUuid() + " not exist");
-        } else {
-            storage[index] = r;
-        }
-    }
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    public Resume[] getAll() {
-        Resume[] resumes = new Resume[size];
-
-        if (size >= 0) System.arraycopy(storage, 0, resumes, 0, size);
-
-        return resumes;
-    }
-
-    public int size() {
-        return size;
+    @Override
+    protected void insertElement(Resume r, int index) {
+        storage[size] = r;
     }
 
     protected int getIndex(String uuid) {
